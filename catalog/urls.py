1eb2d5bebb8f324bec01b67c53520ffe5ac517b0
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-07 11:52:48 +0530

    Add book renew function

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
 
